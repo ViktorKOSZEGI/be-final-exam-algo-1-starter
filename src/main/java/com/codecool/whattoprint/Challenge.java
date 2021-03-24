e6f742cee3a15c11a880beefc44dd54b7fedd4cc
ViktorKOSZEGI <koszegiv.5@gmail.com>
2021-03-24 09:58:03 +0100

    Implement Challenge.decideAndSay() method

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,7 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        String result = (number % 3 == 0 && number % 7 == 0)? words[0] + words[1] : (number % 3 == 0)? words[0] : (number % 7 == 0)? words[1] : "";
+        return result;
     }
 }
